2ceff29dc07ac267da69629298aed281f6a388b2
candicedd <dufangyu0306@hotmail.com>
2019-11-16 23:31:19 +0100

    get links

--- get_address.py
+++ get_address.py
@@ -5,6 +5,18 @@ start_url = "https://www.navitime.co.jp/?ctl=0050"
 
 page = urllib.request.urlopen(start_url)
 
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(page,features="lxml")
 
-print (soup.prettify())
+pref_list = []
+
+# step1, get prefeture names and links
+for pref in soup.find_all("li", class_="pref-item"):
+    for link in pref.find_all("a"):
+        pref_list.append(link.get("href"))
+
+
+print (pref_list)
+
+# step 2, get city names and links
+for link in pref_list:
+    print (link, file=open("link","a", encoding="utf8"))
