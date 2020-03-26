0fb99a37e05fe4a7b1f73d899baa0a2e123642c1
Cong Lian <liancong22@gmail.com>
2020-03-26 22:04:09 +0100

    added GUI with tkinter

--- client-example.py
+++ client-example.py
@@ -1,7 +1,26 @@
 import http.client
+from tkinter import *
 
+window = Tk()
 conn = http.client.HTTPConnection("localhost", 8080)
-conn.request("GET", "/random-number")
-res = conn.getresponse()
-content = res.read().decode()
-print(content)
+
+window.title("Random Number Generator")
+window.geometry('200x150')
+v = StringVar()
+
+
+def clicked():
+
+    conn.request("GET", "/random-number")
+    res = conn.getresponse()
+    content = res.read().decode().splitlines()
+    v.set(content[-1].strip())
+
+
+btn = Button(window, text="Generate", bg="sky blue", command=clicked)
+btn.pack()
+lbl = Label(window, textvariable=v, fg="purple")
+lbl.config(font=("Courier", 44))
+lbl.pack()
+
+window.mainloop()
